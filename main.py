122fb330ffff46cfdf60f8cc254ce27d999ac700
Fatafim <57731567+Fatafim@users.noreply.github.com>
2024-01-08 21:47:06 +0100

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def start():
+    st.title('Uber pickups in NYC')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    start()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
